2c014e7cea19ef89c332e5267dc7931e5162c6e1
alwaysanan <alwaysanan@gmail.com>
2025-03-06 10:40:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Mengubah teks judul
+document.getElementById("judul").innerHTML = "about anan";
+
+// Event listener untuk tombol PROFIL
+document.getElementById("profil").addEventListener("click", function() {
+  window.open("https://alwaysanan.github.io/profil", "_blank");
+});
+
+// Event listener untuk tombol MOTIVASI
+document.getElementById("contact").addEventListener("click", function() {
+  window.open("https://alwaysanan.github.io/contact", "_blank");
+});
+
+// Event listener untuk tombol HADIST
+document.getElementById("shop").addEventListener("click", function() {
+  window.open("https://alwaysanan.github.io/shop", "_blank");
+});
