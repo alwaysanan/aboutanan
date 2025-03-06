71e83d95c6c184f26d6af3f023e7be49d6001706
alwaysanan <alwaysanan@gmail.com>
2025-03-06 10:39:14 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Mengambil elemen HTML dengan id "judul" dan mengubah teksnya
-document.getElementById("judul").innerHTML = "about anan?";
-
-// Mengambil elemen HTML dengan id "tombol" dan menambahkan event listener
-document.getElementById("tombol").addEventListener("click", function() {
-  window.open("https://alwaysanan.github.io/profil", "_blank");
-});
